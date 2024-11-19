55359ef9c462ab8eaebd8eb34ab031189fbe204e
arshiya <arshiyazade@gmail.com>
2024-11-19 12:07:03 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import { StandardComponent } from './standard/standard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: 'standard', component: StandardComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
